49770a4a6a5c89eac944d767c1dff064a5e968cb
aleks <aleksey9045@gmail.com>
2021-11-28 12:16:59 +0300

    coffee ver 3.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow
 
 
 class MyWidget(QMainWindow):
@@ -13,21 +14,17 @@ class MyWidget(QMainWindow):
         self.con = sqlite3.connect("cofe.sqlite")
         self.setWindowTitle('Coffee')
         self.pushButton.clicked.connect(self.update_result)
-        self.modified = {}
 
     def update_result(self):
-        cur = self.con.cursor()
-        # Получили результат запроса, который ввели в текстовое поле
-        result = cur.execute("SELECT * FROM coffee").fetchall()
-        # Заполнили размеры таблицы
-        self.tableWidget.setRowCount(len(result))
-        self.tableWidget.setColumnCount(len(result[0]))
-        self.titles = [description[0] for description in cur.description]
-        # Заполнили таблицу полученными элементами
-        for i, elem in enumerate(result):
-            for j, val in enumerate(elem):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        self.modified = {}
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee.sqlite')
+        db.open()
+        model = QSqlTableModel(self, db)
+        model.setTable('coffee')
+        model.select()
+        # Для отображения данных на виджете
+        # свяжем его и нашу модель данных
+        self.view.setModel(model)
 
 
 if __name__ == '__main__':
