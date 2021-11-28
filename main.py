340cb5d0672677afc98a8d5475ad6b68c4e660ae
aleks <aleksey9045@gmail.com>
2021-11-28 10:37:30 +0300

    coffee ver. 0,1

---
+++ main.py
@@ -0,0 +1,37 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self)
+        self.con = sqlite3.connect("cofe.sqlite")
+        self.setWindowTitle('Coffee')
+        self.pushButton.clicked.connect(self.update_result)
+        self.modified = {}
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM coffee").fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
